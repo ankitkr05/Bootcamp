7ecbccef6a0b031a106950ab0c8b976fd3108c8a
ankitkr05 <95744856+ankitkr05@users.noreply.github.com>
2023-07-27 17:19:27 +0530

    Add files via upload

---
+++ Java (Day4)/AccessModifier/B.java
@@ -0,0 +1,14 @@
+package module1;
+
+public class B {
+
+	public static void main(String[] args) {
+		
+		A o = new A();
+		System.out.println(o.b);
+		System.out.println(o.c);
+		System.out.println(o.d);
+
+	}
+
+}
---
+++ Java (Day4)/AccessModifier/C.java
@@ -0,0 +1,13 @@
+package module2;
+
+import module1.A;
+
+public class C {
+
+	public static void main(String[] args) {
+		
+		A o = new A();
+		System.out.println(o.d);
+	}
+
+}
