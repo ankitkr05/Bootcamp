c7ef54af17743d0e8a3e9c85ee4f524bd91aad45
ankitkr05 <95744856+ankitkr05@users.noreply.github.com>
2023-08-17 11:54:25 +0530

    Add files via upload

---
+++ JavaScript (Day 16-17)/array.js
@@ -0,0 +1,16 @@
+// literal
+var marks = [34, 56, 78, 90, 45];
+console.log(marks);
+console.log("--------");
+
+// new keyword
+var marks2 = new Array();
+marks2[0] = 67;
+marks2[1] = 90;
+marks2[4] = 50;
+console.log(marks2);
+
+
+// Array Constructor
+var marks3 = new Array(34,92,78,54);
+console.log(marks3);
---
+++ JavaScript (Day 16-17)/myscript.js
@@ -0,0 +1,9 @@
+var a = 1;
+var b = 2;
+
+console.log(a + b);
+console.log("External JavaScript");
+
+for(let i=1; i<=10; i++){
+    console.log("Hello " + i);
+}
